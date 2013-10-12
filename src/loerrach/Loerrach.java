65f3e50e77ef6662420cdfdc1b728b3c65464b16
clemenswb <clemens.weber@hornecker.de>
2013-10-12 16:17:43 +0200

    test hallo
    test

---
+++ src/loerrach/Loerrach.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loerrach;
+
+/**
+ *
+ * @author cleme_000
+ */
+public class Loerrach {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //test
+    }
+}
